f00149fefc72f6482f5275116e7959f9a5cb3146
Anwar Elaajabi <anwar.elaajabi1999@gmail.com>
2025-07-30 21:50:00 +0100

    project files set up

---
+++ lib/color_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ColorScreen extends StatelessWidget {
+  const ColorScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
